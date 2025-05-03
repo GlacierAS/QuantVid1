5e755448b48e365e7d4f0ae321634c24231dda7f
GlacierAS <foreverfly496@gmail.com>
2025-05-03 01:53:14 -0400

    Animated lines

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 from manim import *
 
-class AnimatedSquareToCircle(Scene):
+class Vid(Scene):
     def construct(self):
         #self.quoteAnimation()
-        self.intro()
+        #self.intro()
+        es = self.getElectronSpectrum()
 
-
+        self.play(es.animate.shift(RIGHT))
         self.wait(5)
+    
+    def getElectronSpectrum(self):
+        lines = [
+            Line(LEFT * 2 + UP * 1, RIGHT * 2 + UP * 1),
+            Line(LEFT * 2 + UP * 0.5, RIGHT * 2 + UP * 0.5),
+            Line(LEFT * 2, RIGHT * 2),
+            Line(LEFT * 2 + DOWN * 0.5, RIGHT * 2 + DOWN * 0.5),
+            Line(LEFT * 2 + DOWN * 1, RIGHT * 2 + DOWN * 1),
+        ]
+
+        labels = [
+            Text(f"n={5-i}", color=WHITE)
+            .scale(0.6).next_to(line, LEFT, buff=0.2)
+            for i, line in enumerate(lines)
+        ]
+
+        # Pair each line and label animation
+        animations = [
+            Succession(
+                Create(line, run_time=0.3),
+                FadeIn(label, run_time=0.2)
+            )
+            for line, label in zip(lines, labels)
+        ]
+
+        self.play(AnimationGroup(*animations, lag_ratio=0.1))
+
+        return VGroup(lines + labels)
 
     def getElectron(self):
-        c = Circle(radius=2, color=RED, fill_opacity=0.5)
+        c = Circle(radius=2, color=RED, fill_opacity=0.5).scale(0.1)
         c.set_stroke(color=RED, width=6)
 
-        minus_sign = Text('-', color=WHITE).scale(2).move_to(ORIGIN)
-        electron = VGroup(c, minus_sign)
+        minus_sign = Text('-', color=WHITE).move_to(ORIGIN)
+        
+        electron =  VGroup(c, minus_sign)
+        self.play(FadeIn(electron, run_time=0.2))
+        return electron
 
-        self.play(electron.animate.fade(0))
     def intro(self):
         eV13_6 = MathTex(r"-13.6\; \text{eV}")
         e_level_eq = MathTex(
             r"\Delta E = -13.6eV \left( \frac{1}{n^2_{f}}-\frac{1}{n_{i}^2} \right)"
-            ).shift(DOWN * 0.8)
+            ).shift(DOWN * 0.6)
         self.play(Create(eV13_6))
         self.wait(0.5)
-        self.play(eV13_6.animate.shift(UP * 0.8))
+        self.play(eV13_6.animate.shift(UP * 0.6))
         self.play(Create(e_level_eq))
-        self.wait(5)
 
         self.getElectron()
 
